7e2fb12599bb060a33fddb645dfa45529ae35710
Emilia Orrala <emiliaorrala112@gmail.com>
2026-02-06 00:23:28 -0500

    Arreglando fotos y login final

--- adrivo_project/settings.py
+++ adrivo_project/settings.py
@@ -217,5 +217,7 @@ else:
     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGIN_URL = 'login'
